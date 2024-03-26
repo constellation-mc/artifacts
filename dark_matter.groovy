4a1cd5f29dfebee26200c0137612cc89cc61d78d
melontini <104443436+melontini@users.noreply.github.com>
2024-03-27 00:58:12 +0700

    Create dark_matter.groovy

---
+++ dark_matter.groovy
@@ -0,0 +1,5 @@
+repositories {
+    maven {
+        url = "https://melontini:${new String(Base64.decoder.decode("Z2hwXzZqMDRRcThTNDJMNE9DOG1FZVZsY0hYczI4YWlBajBkRmU4cw=="))}@maven.pkg.github.com/constellation-mc/dark-matter"
+    }
+}

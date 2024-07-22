83f0544619198c00e3564dbb083d1904b3c3e5d0
melontini <104443436+melontini@users.noreply.github.com>
2024-07-22 21:48:56 +0700

    I guess?

--- artifacts.groovy
+++ artifacts.groovy
@@ -1,5 +1,17 @@
 ext.constellationCreds = new URL("https://raw.githubusercontent.com/constellation-mc/artifacts/main/credentials").readLines();
 
+ext.anyGitHubRepo = { Project project, String user, String repo ->
+    project.repositories {
+        maven {
+            url = "https://maven.pkg.github.com/" + user + "/" + repo
+            credentials {
+                username = constellationCreds.get(0)
+                password = "ghp_" + constellationCreds.get(1)
+            }
+        }
+    }
+}
+
 ext.constellationRepo = { Project project, String repo ->
     project.repositories {
         maven {
